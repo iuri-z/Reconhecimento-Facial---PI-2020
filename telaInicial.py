235c0bfa4ecf4eaaa103f2b683d4c17362df6e26
pyOliver <oliverpenedo@gmail.com>
2021-06-23 18:20:32 -0300

    add icon

--- telaInicial.py
+++ telaInicial.py
@@ -3,10 +3,10 @@ from tkinter import *
 import os
 from PIL import ImageTk, Image
 
-
-#Criando a tela
+#Criando e configurando a tela
 root = Tk(className=' Tela Inicial')
 root.resizable(0,0)
+
 canvas = Canvas(root, width=600, height=600)
 canvas.grid(columnspan=5,rowspan=16)
 
@@ -21,6 +21,9 @@ def onBotConfig():
 def onBotSair():
 	quit()
 
+icon = PhotoImage(file='Imagens/logoPI_v5.png')
+root.iconphoto(False, icon)
+
 #Criando os botões 'Iniciar', 'Configurações' e 'Sair'
 bot1 = Button(root, text="Iniciar", borderwidth=0, height=2, width=15, fg="white", bg="black", font=('Raleway', 10, 'bold'), command=onBotIniciar) 
 bot2 = Button(root, text="Configurações", borderwidth=0, height=2, width=15, fg="white", bg="black", font=('Raleway', 10, 'bold'), command=onBotConfig) 
@@ -31,7 +34,6 @@ img_logo = Image.open("Imagens/logoPI_v6.png")
 resize_logo = img_logo.resize((300,300), Image.ANTIALIAS)
 get_logo = ImageTk.PhotoImage(resize_logo)
 logo = Label(root, image=get_logo)
-
 
 #Posicionando o necessário na tela
 logo.grid(row=3,column=2)
