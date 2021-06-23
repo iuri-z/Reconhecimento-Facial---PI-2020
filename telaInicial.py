b16e3b69a4da9f33442ed6edd6c08126519cee0f
pyOliver <oliverpenedo@gmail.com>
2021-06-23 16:43:59 -0300

    re_org#4

--- telaInicial.py
+++ telaInicial.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 from tkinter import *
 import os
+from PIL import ImageTk, Image
 
 
 #Criando a tela
-root = Tk(className='Reconhecimento Facial - Tela Inicial')
-canvas = Canvas(root, width=400, height=400)
-canvas.grid(columnspan=5,rowspan=9)
+root = Tk(className=' Tela Inicial')
+root.resizable(0,0)
+canvas = Canvas(root, width=600, height=600)
+canvas.grid(columnspan=5,rowspan=16)
 
 
 #Criando as funções para cada botão
@@ -14,23 +16,29 @@ def onBotIniciar():
 	os.system('python detect_mask_video.py')
 
 def onBotConfig():
-	os.system('python config.py')
+	os.system('python telaConfig.py')
 
 def onBotSair():
 	quit()
 
-
 #Criando os botões 'Iniciar', 'Configurações' e 'Sair'
-bot1 = Button(root, text="Iniciar", font="Stark", borderwidth=0,	width=15, command=onBotIniciar) 
-bot2 = Button(root, text="Configurações", font="Stark", borderwidth=0, width=15, command=onBotConfig) 
-bot3 = Button(root, text="Sair", font="Stark", borderwidth=0, width=15, command=onBotSair)
+bot1 = Button(root, text="Iniciar", borderwidth=0, height=2, width=15, fg="white", bg="black", font=('Raleway', 10, 'bold'), command=onBotIniciar) 
+bot2 = Button(root, text="Configurações", borderwidth=0, height=2, width=15, fg="white", bg="black", font=('Raleway', 10, 'bold'), command=onBotConfig) 
+bot3 = Button(root, text="Sair", borderwidth=0, height=2, width=15, fg="white", bg="black", font=('Raleway', 10, 'bold'), command=onBotSair)
 
-	
-#Posicionando os botões na tela
-bot1.grid(row=3, column=2)
-bot2.grid(row=4, column=2)
-bot3.grid(row=5, column=2)
+#Inserindo a logo
+img_logo = Image.open("Imagens/logoPI_v6.png")
+resize_logo = img_logo.resize((300,300), Image.ANTIALIAS)
+get_logo = ImageTk.PhotoImage(resize_logo)
+logo = Label(root, image=get_logo)
 
+
+#Posicionando o necessário na tela
+logo.grid(row=3,column=2)
+
+bot1.grid(row=7, column=2)
+bot2.grid(row=8, column=2)
+bot3.grid(row=9, column=2)
 
 #Rodando o software
 root.mainloop()
